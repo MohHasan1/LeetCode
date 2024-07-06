e2e9264706cc92250bced4cad0766524d5a37725
Hasan <hemailhasan88@gmail.com>
2024-07-06 23:24:10 +0300

    Time: 2 ms (54.3%), Space: 7.2 MB (31.2%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int passThePillow(int n, int time) {
 
-        // for (int i = 0; i < n; i++) {
-        //     // cout << i + 1 << endl;
-        //     for (int j = 0; j<time; j++){
+        int currPerson = 1;    // starts with first person
+        int passdirection = 1; // backward = -1
 
-        //     }
-        // }
+        for (int currTime = 0; currTime < time; currTime++) { // while time elapses.
 
-        int currPerson = 1;
-        int passdirection = 1; //-1
-
-        for (int currTime = 0; currTime < time; currTime++) {
             currPerson += passdirection;
 
+            // if we reach the last person and currTime
+            // is still < time, reverse the direction (-1)
             if (currPerson == n) {
                 passdirection = -1;
             }
 
+            // if we reach the first person again and currTime
+            // is still < time, reverse the direction again (1)
             if (currPerson == 1) {
                 passdirection = 1;
             }
