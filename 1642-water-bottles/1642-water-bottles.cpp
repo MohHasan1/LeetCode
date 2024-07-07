1b82580d13150badcd76173040e03c1194d2ac20
Hasan <hemailhasan88@gmail.com>
2024-07-07 23:28:46 +0300

    Time: 9 ms (13.73%), Space: 7.2 MB (30.83%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        // int exchnageBottle = 0;
+        int exchnageBottle = 0;
 
-        // for (int i = 1; i <= numBottles + exchnageBottle; i++) {
-        //     if (i / numExchange == 1) {
-        //        exchnageBottle++;
-        //        cout << exchnageBottle <<endl;
+        for (int i = 1; i <= numBottles + exchnageBottle; i++) {
+            cout << i << endl;
+            if (i % numExchange == 0) {
+                exchnageBottle++;
+                //    cout << exchnageBottle <<endl;
+            }
+        }
+
+        // for (int i = 0; i < exchnageBottle; i++) {
+        //     if (i % numExchange == 0) {
+        //         exexBottle++;
         //     }
         // }
 
-        // // for (int i = 0; i < exchnageBottle; i++) {
-        // //     if (i % numExchange == 0) {
-        // //         exexBottle++;
-        // //     }
-        // // }
+        return numBottles + exchnageBottle;
 
-        // return numBottles + exchnageBottle;
+        // int totalDrunk = numBottles;
+        // int emptyBottles = numBottles;
 
-        int totalDrunk = numBottles;
-        int emptyBottles = numBottles;
+        // while (emptyBottles >= numExchange) {
+        //     int newBottles = emptyBottles / numExchange;
+        //     totalDrunk += newBottles;
+        //     emptyBottles = emptyBottles % numExchange + newBottles;
+        // }
 
-        while (emptyBottles >= numExchange) {
-            int newBottles = emptyBottles / numExchange;
-            totalDrunk += newBottles;
-            emptyBottles = emptyBottles % numExchange + newBottles;
-        }
-
-        return totalDrunk;
+        // return totalDrunk;
     }
 };
