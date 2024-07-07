9faa021d26c28f76b2f14a06e27d5e4440cdeb1f
Hasan <hemailhasan88@gmail.com>
2024-07-07 23:24:38 +0300

    Time: 2 ms (54.02%), Space: 7.2 MB (30.83%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        // int exchnageBottle = 0;
+
+        // for (int i = 1; i <= numBottles + exchnageBottle; i++) {
+        //     if (i / numExchange == 1) {
+        //        exchnageBottle++;
+        //        cout << exchnageBottle <<endl;
+        //     }
+        // }
+
+        // // for (int i = 0; i < exchnageBottle; i++) {
+        // //     if (i % numExchange == 0) {
+        // //         exexBottle++;
+        // //     }
+        // // }
+
+        // return numBottles + exchnageBottle;
+
+        int totalDrunk = numBottles;
+        int emptyBottles = numBottles;
+
+        while (emptyBottles >= numExchange) {
+            int newBottles = emptyBottles / numExchange;
+            totalDrunk += newBottles;
+            emptyBottles = emptyBottles % numExchange + newBottles;
+        }
+
+        return totalDrunk;
+    }
+};
