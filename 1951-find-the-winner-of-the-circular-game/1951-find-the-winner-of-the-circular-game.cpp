a77ef6f4db2e400eecaadc80ff1144420ca9b7c0
Hasan <hemailhasan88@gmail.com>
2024-07-08 23:11:20 +0300

    Time: 0 ms (100%), Space: 7.1 MB (92.64%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -6,11 +6,16 @@ public:
             return n;
         if (n == 0)
             return 0;
+
+        // % method
         int winner = 0;
         for (int i = 2; i <= n; i++) {
             winner = (winner + k) % i;
         }
         // Convert to 1-based index
         return winner + 1;
+
+        // queue method
+        //...
     }
 };
