f8f4da4f4644449f913d7ce94576a6882dae863d
Hasan <hemailhasan88@gmail.com>
2024-07-12 03:03:04 +0300

    Time: 28 ms (49.76%), Space: 30.2 MB (24.27%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -7,15 +7,14 @@ public:
 
         // divide and conqur
         while (left <= right) {
-            int middle = left + (right - left) / 2;
             if (target == nums[middle]) {
                 return middle;
             } else if (target > nums[middle]) {
                 left = middle + 1;
-                middle = (right - left) / 2;
+                middle = left +  (right - left) / 2;
             } else if (target < nums[middle]) {
                 right = middle - 1;
-                middle = (right - left) / 2;
+                middle =  left + (right - left) / 2;
             }
         }
 
